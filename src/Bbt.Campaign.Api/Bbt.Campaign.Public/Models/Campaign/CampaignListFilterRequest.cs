7a552e3a8ed24a7aaef014ab5104f7e7876e8927
Safrantek\İSMAİL YAZAR <ismail.yazar@safrantek.com>
2022-04-14 19:23:56 +0300

    test

--- src/Bbt.Campaign.Api/Bbt.Campaign.Public/Models/Campaign/CampaignListFilterRequest.cs
+++ src/Bbt.Campaign.Api/Bbt.Campaign.Public/Models/Campaign/CampaignListFilterRequest.cs
@@ -5,8 +5,6 @@ namespace Bbt.Campaign.Public.Models.Campaign
     public class CampaignListFilterRequest : PagingRequest
     {
         public string? Name { get; set; }
-
-
         public string? Code { get; set; }
         public int? ContractId { get; set; }
         public bool? IsActive { get; set; }
