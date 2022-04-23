25193e175e6ebe1e815b1ca0770ecafbdf3df222
fatih-ozen-safrantek <fatih.ozen@safrantek.com>
2022-04-23 16:35:33 +0300

    Kampanya kazanımlar sekmesi yeniden değiştirildi.

--- src/Ui/src/app/screens/campaign-definition-container/campaign-definition/campaign-gains/campaign-gains.component.ts
+++ src/Ui/src/app/screens/campaign-definition-container/campaign-definition/campaign-gains/campaign-gains.component.ts
@@ -4,13 +4,17 @@ import {StepService} from "../../../../services/step.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {GlobalVariable} from "../../../../global";
-import {CampaignDefinitionGainsAddUpdateRequestModel} from "../../../../models/campaign-definition";
+import {
+  CampaignDefinitionGainModel,
+  CampaignDefinitionGainsAddUpdateRequestModel
+} from "../../../../models/campaign-definition";
 import {DropdownListModel} from "../../../../models/dropdown-list.model";
-import {Subject, take, takeUntil} from "rxjs";
+import {Subject, takeUntil} from "rxjs";
 import {ToastrHandleService} from 'src/app/services/toastr-handle.service';
 import {NgxSmartModalService} from "ngx-smart-modal";
 import {FormChange} from "../../../../models/form-change";
 import {FormChangeAlertComponent} from "../../../../components/form-change-alert/form-change-alert.component";
+import {UtilityService} from "../../../../services/utility.service";
 
 @Component({
   selector: 'app-campaign-gains',
@@ -27,8 +31,9 @@ export class CampaignGainsComponent implements OnInit, FormChange {
 
   formGroup: FormGroup;
   submitted = false;
+  idEdited = false;
 
-  channelCodeList: DropdownListModel[];
+  campaignAchievementList: any[];
   achievementTypeList: DropdownListModel[];
   actionOptionList: DropdownListModel[];
   currencyList: DropdownListModel[];
@@ -40,6 +45,9 @@ export class CampaignGainsComponent implements OnInit, FormChange {
 
   previewLink = GlobalVariable.preview;
 
+  deletedAchievement: any;
+
+  addUpdateButtonText = 'Ekle';
   nextButtonVisible = true;
   isInvisibleCampaign = false;
   buttonTypeIsContinue = false;
@@ -49,6 +57,7 @@ export class CampaignGainsComponent implements OnInit, FormChange {
   constructor(private fb: FormBuilder,
               private stepService: StepService,
               private modalService: NgxSmartModalService,
+              private utilityService: UtilityService,
               private toastrHandleService: ToastrHandleService,
               private campaignDefinitionService: CampaignDefinitionService,
               private router: Router,
@@ -65,7 +74,7 @@ export class CampaignGainsComponent implements OnInit, FormChange {
     if (this.id) {
       this.campaignDefinitionService.repostData.id = this.id;
 
-      this.getCampaignDefinitionGain();
+      this.campaignDefinitionGainsGetUpdateForm();
 
       this.stepService.finish();
 
@@ -74,12 +83,12 @@ export class CampaignGainsComponent implements OnInit, FormChange {
         this.nextButtonVisible = true;
       }
     } else {
-      this.getCampaignDefinitionGainsGetInsertForm();
       this.formChangeState = true;
     }
 
     this.formGroup = this.fb.group({
-      campaignChannelCodeList: [[], Validators.required],
+      id: 0,
+      fakeId: null,
       type: 1,
       achievementTypeId: [null, Validators.required],
       actionOptionId: null,
@@ -159,64 +168,11 @@ export class CampaignGainsComponent implements OnInit, FormChange {
     });
   }
 
-  private populateForm(data) {
-    this.formGroup.patchValue({
-      campaignChannelCodeList: data.channelCodeList,
-      type: data.type,
-      achievementTypeId: data.achievementTypeId,
-      actionOptionId: data.actionOptionId,
-      titleTr: data.titleTr,
-      titleEn: data.titleTr,
-      descriptionTr: data.descriptionTr,
-      descriptionEn: data.descriptionEn,
-      currencyId: data.currencyId,
-      maxAmount: data.maxAmount,
-      amount: data.amount,
-      rate: data.rate,
-      maxUtilization: data.maxUtilization,
-    })
-  }
-
-  private populateLists(data: any) {
-    this.channelCodeList = data.channelCodeList;
-    this.achievementTypeList = data.achievementTypes;
-    this.actionOptionList = data.actionOptions;
-    this.currencyList = data.currencyList;
-  }
-
-  private createRequestModel() {
-    let formGroup = this.formGroup.getRawValue();
-    let requestModel = new CampaignDefinitionGainsAddUpdateRequestModel();
-
-    requestModel.campaignId = this.id ?? this.newId;
-    requestModel.campaignChannelCodeList = formGroup.campaignChannelCodeList;
-    requestModel.type = formGroup.type;
-    requestModel.achievementTypeId = formGroup.achievementTypeId;
-    requestModel.actionOptionId = formGroup.actionOptionId;
-    requestModel.titleTr = formGroup.titleTr;
-    requestModel.titleEn = formGroup.titleEn;
-    requestModel.descriptionTr = formGroup.descriptionTr;
-    requestModel.descriptionEn = formGroup.descriptionEn;
-    requestModel.maxUtilization = parseInt(formGroup.maxUtilization);
-    switch (formGroup.type) {
-      case 1:
-      case "1":
-        requestModel.currencyId = formGroup.currencyId;
-        requestModel.amount = formGroup.amount;
-        requestModel.maxAmount = formGroup.maxAmount;
-        break;
-      case 2:
-      case "2":
-        requestModel.rate = formGroup.rate;
-        break;
-    }
-    return requestModel;
-  }
-
   save() {
-    this.submitted = true;
-    if (this.formGroup.valid) {
-      this.id ? this.campaignDefinitionGainsUpdate() : this.campaignDefinitionGainsAdd();
+    if (this.campaignAchievementList.length > 0) {
+      this.campaignDefinitionGainsUpdate();
+    } else {
+      this.toastrHandleService.warning("Kazanım girilmelidir.");
     }
   }
 
@@ -239,68 +195,163 @@ export class CampaignGainsComponent implements OnInit, FormChange {
       : this.router.navigate([`/campaign-definition/update/${this.id}/finish`], {relativeTo: this.route});
   }
 
+  private clearForm() {
+    this.formGroup.patchValue({
+      id: 0,
+      fakeId: this.utilityService.createGuid(),
+      type: 1,
+      achievementTypeId: null,
+      actionOptionId: null,
+      titleTr: '',
+      titleEn: '',
+      descriptionTr: '',
+      descriptionEn: '',
+      currencyId: 1,
+      maxAmount: null,
+      amount: null,
+      rate: null,
+      maxUtilization: '',
+    })
+  }
+
+  private populateForm(data) {
+    this.formGroup.patchValue({
+      id: data.id,
+      fakeId: data.fakeId ?? this.utilityService.createGuid(),
+      type: data.type,
+      achievementTypeId: data.achievementTypeId,
+      actionOptionId: data.actionOptionId,
+      titleTr: data.titleTr,
+      titleEn: data.titleTr,
+      descriptionTr: data.descriptionTr,
+      descriptionEn: data.descriptionEn,
+      currencyId: data.currencyId,
+      maxAmount: data.maxAmount,
+      amount: data.amount,
+      rate: data.rate,
+      maxUtilization: data.maxUtilization,
+    })
+  }
+
+  private populateLists(data: any) {
+    this.achievementTypeList = data.achievementTypes;
+    this.actionOptionList = data.actionOptions;
+    this.currencyList = data.currencyList;
+  }
+
+  private createAchievement() {
+    let formGroup = this.formGroup.getRawValue();
+    let achievement = new CampaignDefinitionGainModel();
+
+    achievement.id = this.idEdited ? formGroup.id : 0;
+    achievement.fakeId = formGroup.fakeId;
+    achievement.campaignId = this.id ?? this.newId;
+    achievement.type = formGroup.type;
+    achievement.achievementTypeId = formGroup.achievementTypeId;
+    achievement.actionOptionId = formGroup.actionOptionId;
+    achievement.titleTr = formGroup.titleTr;
+    achievement.titleEn = formGroup.titleEn;
+    achievement.descriptionTr = formGroup.descriptionTr;
+    achievement.descriptionEn = formGroup.descriptionEn;
+    achievement.maxUtilization = formGroup.maxUtilization == "" || formGroup.maxUtilization == null
+      ? null
+      : parseInt(formGroup.maxUtilization);
+    switch (formGroup.type) {
+      case 1:
+      case "1":
+        achievement.currencyId = formGroup.currencyId;
+        achievement.amount = formGroup.amount;
+        achievement.maxAmount = formGroup.maxAmount;
+        break;
+      case 2:
+      case "2":
+        achievement.rate = formGroup.rate;
+        break;
+    }
+    return achievement;
+  }
+
+  private populateTableColumn(achievement) {
+    achievement.rule = achievement.type == 1 ? {id: 1, name: 'Tutar'} : {id: 2, name: 'Oran'};
+    achievement.achievementType = this.achievementTypeList.find(x => x.id == achievement.achievementTypeId);
+    achievement.actionOption = this.actionOptionList.find(x => x.id == achievement.actionOptionId);
+
+    return achievement;
+  }
+
+  showAddModal() {
+    this.clearForm();
+    this.typeChanged();
+    this.submitted = false;
+    this.idEdited = false;
+    this.addUpdateButtonText = "Ekle";
+    this.modalService.getModal('campaignGainsAddUpdateModal').open();
+  }
+
+  showUpdateModal(achievement) {
+    this.populateForm(achievement);
+    this.typeChanged();
+    this.submitted = false;
+    this.idEdited = true;
+    this.addUpdateButtonText = "Güncelle";
+    this.modalService.getModal('campaignGainsAddUpdateModal').open();
+  }
+
+  update() {
+    this.submitted = true;
+    if (this.formGroup.valid) {
+      let achievement = this.createAchievement();
+      achievement = this.populateTableColumn(achievement);
+      this.idEdited ? this.updateAchievement(achievement) : this.addAchievement(achievement);
+      this.modalService.getModal('campaignGainsAddUpdateModal').close();
+      this.nextButtonVisible = true;
+      this.formChangeState = true;
+    }
+  }
+
+  addAchievement(achievement) {
+    this.campaignAchievementList.push(achievement);
+  }
+
+  updateAchievement(achievement) {
+    this.campaignAchievementList.splice(
+      this.campaignAchievementList.findIndex(x => x.fakeId == achievement.fakeId), 1, achievement);
+  }
+
+  openDeleteAlertModal(achievement) {
+    this.deletedAchievement = achievement;
+    this.modalService.getModal('campaignGainsDeleteAlertModal').open();
+  }
+
+  closeDeleteAlertModal() {
+    this.modalService.getModal('campaignGainsDeleteAlertModal').close();
+  }
+
+  deleteAlertModalOk() {
+    this.deleteAchievement();
+    this.modalService.getModal('campaignGainsDeleteAlertModal').close();
+  }
+
+  deleteAchievement() {
+    this.campaignAchievementList.splice(this.campaignAchievementList.findIndex(x => x == this.deletedAchievement), 1);
+    this.nextButtonVisible = true;
+    this.formChangeState = true;
+  }
+
   copyCampaign(event) {
     this.campaignDefinitionService.copyCampaign(event.id);
   }
 
-  private getCampaignDefinitionGainsGetInsertForm() {
-    let campaignId = parseInt(this.newId);
-    this.campaignDefinitionService.getCampaignDefinitionGainsGetInsertForm(campaignId)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: res => {
-          if (!res.hasError && res.data) {
-            this.populateLists(res.data);
-            this.campaignViewingStateActions(res.data.isInvisibleCampaign);
-            this.typeChanged();
-          } else
-            this.toastrHandleService.error(res.errorMessage);
-        },
-        error: err => {
-          if (err.error)
-            this.toastrHandleService.error(err.error);
-        }
-      });
-  }
-
-  private getCampaignDefinitionGain() {
+  private campaignDefinitionGainsGetUpdateForm() {
     let campaignId = parseInt(this.id);
-    this.campaignDefinitionService.getCampaignDefinitionGain(campaignId)
+    this.campaignDefinitionService.campaignDefinitionGainsGetUpdateForm(campaignId)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: res => {
           if (!res.hasError && res.data) {
             this.populateLists(res.data);
-            if (res.data.campaignAchievement) {
-              this.populateForm(res.data.campaignAchievement);
-            }
+            this.campaignAchievementList = res.data.campaignAchievementList;
             this.campaignViewingStateActions(res.data.isInvisibleCampaign);
-            this.typeChanged();
-            this.formGroup.valueChanges
-              .pipe(take(1))
-              .subscribe(x => {
-                this.formChangeState = true;
-                this.nextButtonVisible = true;
-              });
-          } else
-            this.toastrHandleService.error(res.errorMessage);
-        },
-        error: err => {
-          if (err.error)
-            this.toastrHandleService.error(err.error);
-        }
-      });
-  }
-
-  private campaignDefinitionGainsAdd() {
-    let requestModel = this.createRequestModel();
-    this.campaignDefinitionService.campaignDefinitionGainsAdd(requestModel)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: res => {
-          if (!res.hasError && res.data) {
-            this.finish(res.data.campaignId);
-            this.toastrHandleService.success();
           } else
             this.toastrHandleService.error(res.errorMessage);
         },
@@ -312,7 +363,10 @@ export class CampaignGainsComponent implements OnInit, FormChange {
   }
 
   private campaignDefinitionGainsUpdate() {
-    let requestModel = this.createRequestModel();
+    let requestModel: CampaignDefinitionGainsAddUpdateRequestModel = {
+      campaignId: this.id ?? this.newId,
+      campaignAchievementList: this.campaignAchievementList
+    };
     this.campaignDefinitionService.campaignDefinitionGainsUpdate(requestModel)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
--- src/Ui/src/app/services/utility.service.ts
+++ src/Ui/src/app/services/utility.service.ts
@@ -26,6 +26,13 @@ export class UtilityService {
   constructor(private router: Router) {
   }
 
+  createGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
   getSelectedItemsForDropdownList(allList: IDropdownListModel[], selectedList: any[]) {
     return selectedList.map(s => allList.find(a => a.id == s));
   }
