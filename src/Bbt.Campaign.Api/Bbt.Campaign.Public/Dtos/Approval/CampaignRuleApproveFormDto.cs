3e34839c4f4139c91f464380fe10d466a5db5a18
Safrantek\İSMAİL YAZAR <ismail.yazar@safrantek.com>
2022-06-07 14:08:26 +0300

    CampaignRuleApproveFormDto

---
+++ src/Bbt.Campaign.Api/Bbt.Campaign.Public/Dtos/Approval/CampaignRuleApproveFormDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bbt.Campaign.Public.Dtos.Approval
+{
+    public class CampaignRuleApproveFormDto
+    {
+        public string JoinTypeName { get; set; }
+        public string RuleBusinessLines { get; set; }
+        public string RuleBranches { get; set; }
+        public string RuleCustomerTypes { get; set; }
+        public string CampaignStartTermName { get; set; }
+    }
+}
