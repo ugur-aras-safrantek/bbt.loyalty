1823ec966a70db34d60688a016c438f762ba15ca
Safrantek\İSMAİL YAZAR <ismail.yazar@safrantek.com>
2022-07-04 15:08:13 +0300

    approval bug fix

--- src/Bbt.Campaign.Api/Bbt.Campaign.Public/Dtos/Approval/HistoryApproveDto.cs
+++ src/Bbt.Campaign.Api/Bbt.Campaign.Public/Dtos/Approval/HistoryApproveDto.cs
@@ -11,5 +11,8 @@ namespace Bbt.Campaign.Public.Dtos.Approval
         public string? ApprovedBy { get; set; }
         public DateTime? ApprovedDate { get; set; }
         public string? ApprovedDateStr { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string? CreatedOnStr { get; set; }
     }
 }
