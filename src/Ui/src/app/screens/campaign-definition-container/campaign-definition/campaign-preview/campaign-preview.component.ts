c20de30265e75a0fea33829e822b34f7a64b53ed
Mehmet Fatih Özen - Safrantek <mozen@burgan.com.tr>
2022-09-01 16:16:48 +0300

    fortify fix

--- src/Ui/src/app/screens/campaign-definition-container/campaign-definition/campaign-preview/campaign-preview.component.ts
+++ src/Ui/src/app/screens/campaign-definition-container/campaign-definition/campaign-preview/campaign-preview.component.ts
@@ -78,11 +78,12 @@ export class CampaignPreviewComponent implements OnInit {
   }
 
   setRandomColorList() {
-    const letters = '0123456789ABCDEF'
+    const letters = '0123456789ABCDEF';
     for (let i = 0; i < 20; i++) {
       let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+      let randomValue = window.crypto.getRandomValues(new Uint8Array(6));
+      for (let j = 0; j < 6; j++) {
+        color += letters[randomValue[j] % 16];
       }
       this.targetColorList.push(color);
     }
--- src/Ui/src/app/services/utility.service.ts
+++ src/Ui/src/app/services/utility.service.ts
@@ -28,7 +28,7 @@ export class UtilityService {
 
   createGuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+      let r = window.crypto.getRandomValues(new Uint32Array(1))[0] * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
   }
